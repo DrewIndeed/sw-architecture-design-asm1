30deee9557d0ce37752e81214ea2fea50484120c
an.le6 <truongan1730@gmail.com>
2022-11-30 23:26:22 +0700

    6. add logos resource

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import LogoOrg from "./logo-org.png";
+import LogoOrgCropped from "./logo-org-cropped.png";
+import LogoForDark from "./logo-for-dark.png";
+import LogoForDarkWhite from "./logo-for-dark-white.png";
+import LogoForLight from "./logo-for-light.png";
+
+export { LogoOrg, LogoOrgCropped, LogoForDark, LogoForDarkWhite, LogoForLight };
